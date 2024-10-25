738054e6d73a4ef4b7d6bd517839e43b642a7bdd
lawrence-254 <lodhiambo26@gmail.com>
2024-10-25 08:58:18 +0300

    services

--- src/componets/home_page/services.jsx
+++ src/componets/home_page/services.jsx
@@ -11,10 +11,26 @@ export const ServicesComponent = ({image, heading, paragraph}) => {
           <img src={image} alt="" />
         </div>
       </div>
-      <h2>{heading}</h2>
-      <p>
+      <div style={{width: '80%',
+        height: 'auto',
+        color: '#FFFFFF'
+        }}>
+      <h2 style={{fontFamily: 'Saira',
+      fontSize: '36px',
+      fontWeight: '700',
+      lineHeight: '42px',
+      textAlign: 'center'
+      }}>{heading}</h2>
+      <p style={{fontFamily: 'Saira',
+      fontSize: '24px',
+      fontWeight: '500',
+      lineHeight: '36px',
+      textAlign: 'center'
+      }}>
         {paragraph}
       </p>
+      </div>
     </div>
+
   )
 }
