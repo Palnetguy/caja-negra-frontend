6fe4db580b4fd4f3108705aa1e326b01266fb453
lawrence-254 <lodhiambo26@gmail.com>
2024-10-25 10:15:27 +0300

    pservices

--- src/componets/home_page/professionale_service.jsx
+++ src/componets/home_page/professionale_service.jsx
@@ -4,16 +4,34 @@ import '../../styles/home_page.css'
 export const ProfessionaleService = ({img, h2, p}) => {
        
   return (
-<div className="service" >
-          <div className="image" >
-            <div className="bgSpan" ></div>
-            <div className="img" >
-              <img src={img} alt="" />
-            </div>
-          </div>
-          <h2>{h2}</h2>
-          <p>
-            {p}
-          </p>
-        </div>  )
-}
+<div className="service profesional-services" >
+  <div className="image">
+    <div className="bgSpan" ></div>
+    <div className="img" >
+      <img src={img} alt="" />
+    </div>
+  </div>
+  <div style={{
+    marginTop: '1em',
+    color: '#FFFFFF'
+
+  }}>
+    <h2 style={{
+      fontFamily: 'Saira',
+      fontSize: '36px',
+      fontWeight: '700',
+      lineHeight: '42px',
+      textAlign: 'center' 
+    }}>{h2}</h2>
+    <p style={{
+      fontFamily: 'Saira',
+      fontSize: '24px',
+      fontWeight: '500',
+      lineHeight: '36px',
+      textAlign: 'center'   
+    }}>
+      {p}
+    </p>
+  </div>
+</div>
+)}
