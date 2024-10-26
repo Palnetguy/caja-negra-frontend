070bb9684eec0df06ad8308dfe2c44870c9b6513
lawrence-254 <lodhiambo26@gmail.com>
2024-10-26 15:35:48 +0300

    bg

--- src/componets/home_page/remarks.jsx
+++ src/componets/home_page/remarks.jsx
@@ -5,8 +5,10 @@ export const RemarksContainer = ({pBody, pFooter}) => {
   return (
 <div className="remark" >
     <div className="bgSpan" ></div>
+    <div className="inner_div">
         <p>{pBody}</p>
         <p>{pFooter}</p>
     </div>
-    )
+    </div>
+  )
 }
