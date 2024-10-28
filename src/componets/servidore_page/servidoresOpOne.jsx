56948721432c85429559c6cf5c59d49d6dd529df
lawrence-254 <lodhiambo26@gmail.com>
2024-10-28 22:39:38 +0300

    1

--- src/componets/servidore_page/servidoresOpOne.jsx
+++ src/componets/servidore_page/servidoresOpOne.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Link } from 'react-router-dom'
 
 export const ServidoresOpOneComponent = ({h1, h2, liItem, bt}) => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const mobileStyle = {
+        width: '180px',
+        height: '80px',
+        color: 'black',
+        fontFamily: 'Saira',
+        fontWeight: '700',
+        fontSize: '24px',
+        marginTop: '2em',        
+    };
          
   return (
   <div className="servidoresOp_1_each_1" >
@@ -13,7 +32,7 @@ export const ServidoresOpOneComponent = ({h1, h2, liItem, bt}) => {
         ))}
     </ul>
     <Link target="_top" to="/solicitar">
-        <button className="full_gradient_btn" style={{
+        <button className="full_gradient_btn" style={isMobile ? mobileStyle : {
             width: '234px',
             height: '80px',
             color:'black',
