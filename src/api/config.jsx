68302e8702138af23d4a193e80c4c240f4123007
lawrence-254 <lodhiambo26@gmail.com>
2024-11-03 00:57:27 +0300

    be

---
+++ src/api/config.jsx
@@ -0,0 +1,6 @@
+const config = {
+    apiEndpoint: 'https://ceja-negra-backend.up.railway.app/api',
+    apiKey: 'NmHvyO2j.koQSYmPDBgkzYTQsgcuEHG24f3b2MFVb',
+  };
+  
+  export default config;
--- src/pages/config.jsx
+++ src/pages/config.jsx
@@ -1,6 +1,7 @@
-const apiEndpoint = "https://l7i-curious-roebling.circumeo-apps.net/api";
-const apiKey = "8xbkxnul.p9Uf7aRwnsLv9vXk0h7qbUVsqa4dEQ9q";
-
+// const apiEndpoint = "https://l7i-curious-roebling.circumeo-apps.net/api";
+// const apiKey = "8xbkxnul.p9Uf7aRwnsLv9vXk0h7qbUVsqa4dEQ9q";
+const apiEndpoint = 'https://ceja-negra-backend.up.railway.app/api';
+const apiKey = 'NmHvyO2j.koQSYmPDBgkzYTQsgcuEHG24f3b2MFVb';
 const config = { apiEndpoint: apiEndpoint, apiKey: apiKey };
 
 export default config;
